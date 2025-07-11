cf34cf9f28ee5577c90091e09a13e9bb29e2ce77
H-ymt <y.handai1272@gmail.com>
2025-07-11 18:11:03 +0900

    style(front-page.php): Adjust indentation for consistency

--- front-page.php
+++ front-page.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <main>
-    <div class="container">
-        <p class="text">🚀 WordPress development with Vite and Local by Flywheel</p>
-    </div>
+  <div class="container">
+    <p class="text">🚀 WordPress development with Vite and Local by Flywheel</p>
+  </div>
 </main>
 
 <?php get_footer();
