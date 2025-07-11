546fcef03ed23d3a6b53436ac115093c50d3f572
H-ymt <y.handai1272@gmail.com>
2025-07-11 17:03:12 +0900

    feat(header): Add meta tags for SEO and social media sharing

--- header.php
+++ header.php
@@ -4,7 +4,22 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="robots" content="index, follow">
+	<meta property="og:image" content="">
+	<meta property="og:title" content="">
+	<meta property="og:description" content="">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="<?php echo esc_url(home_url('/')); ?>">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:url" content="<?php echo esc_url(home_url('/')); ?>">
+
+	<link rel="canonical" href="<?php echo esc_url(home_url('/')); ?>">
+
+	<title><?php wp_title('|', true, 'right');
+					bloginfo('name'); ?></title>
 
 	<?php wp_head(); ?>
 </head>
