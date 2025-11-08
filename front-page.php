53cf1fd6575c27c4c733f94274b6655e233b4203
H-ymt <y.handai1272@gmail.com>
2025-11-08 14:36:06 +0900

    config: Update Prettier config for Tailwind plugin (HTML/JS only)

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <main>
   <div class="container">
-    <p class="text-blue-500 text-center text-2xl sm:text-4xl leading-10 sm:leading-12">🚀 WordPress development <span class="block">with Vite and Local by Flywheel</span></p>
+    <p class="text-blue-500  text-2xl sm:text-4xl leading-10 sm:leading-12 text-center">🚀 WordPress development <span class="block">with Vite and Local by Flywheel</span></p>
   </div>
 </main>
 
